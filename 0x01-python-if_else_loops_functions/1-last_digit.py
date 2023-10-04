ee38bc71e781dc2e9c70f5087e8988976a0be6d3
Michael Gift <simiyugift@outlook.com>
2023-10-04 17:21:03 +0000

    Fix EOL error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,9 +7,10 @@ if num < 0:
 else:
     last_digit = num % 10
 if last_digit > 5:
-    print(f"Last digit of {num} is {last_digit} and is greater than 5")
+    print(f"Last digit of {num} is {last_digit} and is greater \
+than 5")
 elif last_digit < 6 and last_digit != 0:
-    print(f"Last digit of {num} is {last_digit} and is less than 
+    print(f"Last digit of {num} is {last_digit} and is less than \
 6 and not 0")
 elif last_digit == 0:
     print(f"Last digit of {num} is {last_digit} and is 0")
