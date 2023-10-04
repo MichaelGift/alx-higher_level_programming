9b27b3f9e2b220627d77f37c15652929bfb7c854
Michael Gift <simiyugift@outlook.com>
2023-10-04 18:38:54 +0000

    Add a function to remove char

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    temp_str = ""
+    cont = 0
+    for c in str:
+        if cont == n:
+            pass
+        else:
+            temp_str += c
+        cont += 1
+return temp_str
