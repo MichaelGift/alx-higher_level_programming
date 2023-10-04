5b112c8228bb0323f85f76ddaf2912fc21b1121f
Michael Gift <simiyugift@outlook.com>
2023-10-04 18:07:28 +0000

    Add a func to print upper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-    temp_str = ""
+    strtmp = ""
     for c in str:
         if ord(c) > 96 and ord(c) < 123:
-            temp_str += chr(ord(c)-32)
+            strtmp += chr(ord(c)-32)
         else:
-            temp_str += c
-            print("{}".format(temp_str))
+            strtmp += c
+            print("{}".format(strtmp))
