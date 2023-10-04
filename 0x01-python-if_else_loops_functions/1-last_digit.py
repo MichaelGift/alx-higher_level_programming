16bd013ecfa87e28722c4a140c90b8e40c917b3a
Michael Gift <simiyugift@outlook.com>
2023-10-04 17:24:21 +0000

    Fix random gen mistake

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 import random
-num = random.randint(-10000, 10000)
-if num < 0:
-    first_digit = -num % 10
+number = random.randint(-10000, 10000)
+if number < 0:
+    first_digit = -number % 10
     last_digit = -last_digit
 else:
-    last_digit = num % 10
+    last_digit = number % 10
 if last_digit > 5:
-    print(f"Last digit of {num} is {last_digit} and is greater \
+    print(f"Last digit of {number} is {last_digit} and is greater \
 than 5")
 elif last_digit < 6 and last_digit != 0:
-    print(f"Last digit of {num} is {last_digit} and is less than \
+    print(f"Last digit of {number} is {last_digit} and is less than \
 6 and not 0")
 elif last_digit == 0:
-    print(f"Last digit of {num} is {last_digit} and is 0")
+    print(f"Last digit of {number} is {last_digit} and is 0")
