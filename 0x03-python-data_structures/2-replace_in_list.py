5712d6e18cd9113ff14f371abf6fb929b05d8153
MichaelGift <simiyugift@outlook.com>
2023-10-09 17:01:27 -0700

    Fix implementation

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
+def replace_in_list(my_list, idx, element):
     if 0 <= idx < len(my_list):
-        return my_list[idx]
-    return None
+        my_list[idx] = element
+    return my_list
