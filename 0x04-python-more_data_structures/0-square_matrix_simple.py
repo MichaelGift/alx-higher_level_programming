9b95311fb3f8e82acd742ff1b050ee7809a3cd5e
MichaelGift <simiyugift@outlook.com>
2023-10-11 16:24:11 -0700

    Square a matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    return [[item ** 2 for item  in list_] for list_ in matrix]
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,1 @@
+What is the point of having power if you can't use it?
