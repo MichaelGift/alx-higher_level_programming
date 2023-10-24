6598f4f94868ccb62d836da543820b1630009edb
MichaelGift <simiyugift@outlook.com>
2023-10-24 16:16:55 -0700

    Return size ** 2

---
+++ 0x06-python-classes/3-sqaures.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Square module.
+This module contains a class that defines a square.
+"""
+
+
+class Square():
+    """Defines a square."""
+
+    def __init__(self, size=0):
+        """Sets the necessary attributes for the Square object.
+        Args:
+            size (int): the length of one edge.
+        Raises:
+            TypeError: if size is not given as an integer.
+            ValueError: if size is less than 0.
+        """
+        if type(size) is int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+
+    def area(self):
+        """Returns current square area."""
+        return self.__size ** 2
