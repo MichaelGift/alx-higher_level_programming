6fb284a78914a81977235b36f0c8c8a166ab33c3
MichaelGift <simiyugift@outlook.com>
2023-10-31 12:52:10 -0700

    Add test for integer

---
+++ 0x07-python-test_driven_development/tests/0-add_integer.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Add_integer module.
+Supplies add_integer() function.
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Return the addition of a and b.
+    Type checks the arguments.
+    Args:
+        a (int, float): the first value.
+        b (int, float): the second value.
+    """
+    if type(a) in [int, float]:
+        try:
+            a = int(a)
+        except:
+            raise TypeError('a must be an integer')
+    else:
+        raise TypeError('a must be an integer')
+
+    if type(b) in [int, float]:
+        try:
+            b = int(b)
+        except:
+            raise TypeError('b must be an integer')
+    else:
+        raise TypeError('b must be an integer')
+
+    return a + b
