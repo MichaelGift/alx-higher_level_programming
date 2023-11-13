1186e14b6a485cc2b0c620c4981573f554a680ab
Michael Gift <simiyugift@outlook.com>
2023-11-13 23:09:01 +0000

    Add base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This is the base module
+It contains the "base" to be used throught the project
+"""
+
+
+class Base():
+    """
+    The base of all other classes in the project.
+    Used to manage the id attribute to avoid code duplication.
+    Attributes:
+        __nb_objects (int): Number of Base objects
+    """
+    __nb_objects = 0
+
+
+    def _init_(self, id=None):
+        """
+        Initialized the default attributes
+        Args:
+            id (int): Identifier of the Base object.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
