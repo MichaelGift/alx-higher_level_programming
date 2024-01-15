453e5b34d502830ce35e9653ccc1824bc545d645
MichaelGift <simiyugift@outlook.com>
2024-01-15 04:01:06 +0300

    Load all cities in db

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-Script that lists all `states` from the database `hbtn_0e_0_usa`
-starting with 'N'.
+Script that lists all `cities` from the database `hbtn_0e_4_usa`.
 Arguments:
     mysql username (str)
     mysql password (str)
@@ -16,14 +15,15 @@ if __name__ == "__main__":
     password = sys.argv[2]
     db_name = sys.argv[3]
 
+    # By default, it will connect to localhost:3306
     db = MySQLdb.connect(user=username, passwd=password, db=db_name)
-    cursor = db.cursor()
+    cur = db.cursor()
 
-    cursor.execute("SELECT c.id, c.name, s.name \
-                    FROM cities c INNER JOIN states s \
-                    ON c.state_id = s.id \
-                    ORDER BY c.id")
-    rows = cursor.fetchall()
+    cur.execute("SELECT c.id, c.name, s.name \
+                 FROM cities c INNER JOIN states s \
+                 ON c.state_id = s.id \
+                 ORDER BY c.id")
+    rows = cur.fetchall()
 
     for row in rows:
         print(row)
