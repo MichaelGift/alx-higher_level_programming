2eca843b564bd5d3cf8ac5dd152b6acec3aa40e8
MichaelGift <simiyugift@outlook.com>
2024-02-20 18:04:26 -0800

    Chore: Match linter requirements

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 const content = process.argv[3];
-fs.writeFile(process.argv[2], content,'utf-8', err => {
-    if (err) console.log(err)
+fs.writeFile(process.argv[2], content, 'utf-8', err => {
+  if (err) console.log(err);
 });
