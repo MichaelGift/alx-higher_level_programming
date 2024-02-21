d66e935442059953f67cc654bfe0434850ae243f
MichaelGift <simiyugift@outlook.com>
2024-02-20 18:30:14 -0800

    Chore: Fit linter reqs

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,16 +2,16 @@
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, (error, response, body) => {
-    if (error) return;
-    const characters = JSON.parse(body).characters;
-    printCharacters(characters);
+  if (error) return;
+  const characters = JSON.parse(body).characters;
+  printCharacters(characters);
 });
 
-function printCharacters(characters){
-    characters.forEach((character) => {
-        request(character, (error, response, body) => {
-            if (error) return;
-            console.log(JSON.parse(body).name);
-        });
+function printCharacters (characters) {
+  characters.forEach((character) => {
+    request(character, (error, response, body) => {
+      if (error) return;
+      console.log(JSON.parse(body).name);
     });
+  });
 }
