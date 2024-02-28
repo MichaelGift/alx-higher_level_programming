67c6bfb8bb61a557a7653695568bb721f2de652d
MichaelGift <simiyugift@outlook.com>
2024-02-28 21:42:47 +0300

    Chore: Use correct url

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,6 +1,6 @@
 'use strict';
 $(() => {
-    const BASE_URL = 'https://swapi-api.hbtn.io/api';
+    const BASE_URL = 'https://swapi-api.alx-tools.com/api';
 
     $.get(`${BASE_URL}/people/5/?format=json`, (data, status) => {
         $('DIV#character').html(data.name);
